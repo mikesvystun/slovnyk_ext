f3bc6266ed59d9138b821d8cc6324819fc0a61d4
Mike Svystun <mike@vertalab.com>
2018-03-28 15:52:31 +0300

    play around

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    var i = document.createElement('h1');
-    i.innerHTML = tabs[0].title;
+    var i = document.createElement('p');
+    i.className = 'title';
+    i.innerHTML = tabs[0].title + ' - ' + tabs[0].title.length.toString() + ' symbols';
     var tag = document.getElementsByTagName('body')[0];
     tag.appendChild(i);
   });
