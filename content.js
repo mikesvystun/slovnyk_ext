02edd03f8c48764dc3e9b63e05f85a623e836340
Mike <michael.svystun@gmail.com>
2018-03-28 22:32:46 +0300

    Get page element and send it to api

---
+++ content.js
@@ -0,0 +1,13 @@
+var i = $('h1').text();
+console.log('started');
+console.log(i);
+$.ajax({
+  url: "http://localhost:3000/request",
+  type: "POST", 
+  data: {
+    'original': i
+  },
+  success: function(resp){
+    console.log(resp.translation);
+  },
+});
--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     var i = document.createElement('p');
     i.className = 'title';
-    i.innerHTML = tabs[0].title + ' - ' + tabs[0].title.length.toString() + ' symbols';
+    i.innerHTML = tabs[0].getElementsByTagName('h2')[0].textContent ;
     var tag = document.getElementsByTagName('body')[0];
     tag.appendChild(i);
   });
