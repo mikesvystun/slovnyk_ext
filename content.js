bdf0afbcaadfa487d02ea237cd5b676fbe28bfa4
Mike <michael.svystun@gmail.com>
2018-03-29 22:56:34 +0300

    Don't change element if the api responce object is empty

--- content.js
+++ content.js
@@ -1,6 +1,14 @@
-var i = $('h1').text();
-console.log('started');
-console.log(i);
+function isEmpty(obj) {
+  for(var key in obj) {
+    if(obj.hasOwnProperty(key))
+      return false;
+    }
+  return true;
+}
+
+
+var i = $('h1').text().toLowerCase();
+console.log("to replace: " + i);
 $.ajax({
   url: "http://localhost:3000/request",
   type: "POST", 
@@ -8,6 +16,15 @@ $.ajax({
     'original': i
   },
   success: function(resp){
-    console.log(resp);
+    console.log(isEmpty(resp))
+    if (!isEmpty(resp)) {
+      for (var prop in resp) {
+        console.log("prop: " + prop)
+        console.log("val: " + resp[prop])
+        i = i.replace(prop, resp[prop] );
+      };
+      console.log("result: " + i);
+      $('h1').css("text-decoration", "line-through").after('<h1>' + i + '</h1>');
+    };
   },
 });
