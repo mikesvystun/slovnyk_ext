88701386d0ce853398144f926124b431e1233d52
Mike Svystun <mike@vertalab.com>
2018-04-02 11:16:44 +0300

    On selection works

--- content.js
+++ content.js
@@ -1,6 +1,26 @@
-var i = $('h1').text();
-console.log('started');
-console.log(i);
+function getSelectionText() {
+  var text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
+  return text;
+}
+
+function isEmpty(obj) {
+  for(var key in obj) {
+  if(obj.hasOwnProperty(key))
+    return false;
+  }
+    return true;
+}
+
+
+$('body').mouseup(function() {
+var i = getSelectionText();
+
+
 $.ajax({
   url: "http://localhost:3000/request",
   type: "POST", 
@@ -8,6 +28,18 @@ $.ajax({
     'original': i
   },
   success: function(resp){
-    console.log(resp.translation);
+    console.log(isEmpty(resp))
+    if (!isEmpty(resp)) {
+      for (var prop in resp) {
+        console.log("prop: " + prop)
+        console.log("val: " + resp[prop])
+        i = i.replace(prop, resp[prop] );
+      };
+    console.log("result: " + i);
+    i.parent().after('<h1>' + i + '</h1>');
+    };
   },
+});
+
+
 });
