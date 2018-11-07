c9dc66624fb41aaceee613898b615b9009222c65
Mike Svystun <michael.svystun@gmail.com>
2018-11-07 22:40:32 +0200

    fixing a bug

--- content.js
+++ content.js
@@ -65,35 +65,37 @@ function syncSlovnyk() {
 function replaceLoanwords(text, result) {
   for (var key in result) {
     if (key == 'localsize' || key == 'switcher' ) { continue; };
-    // lowercase ending with a space
-    text = text.replace( key + " ", "<span style='color:green'>" + result[key] + "</span> " );
-    // lowercase ending with a .
-    text = text.replace( key + ".", "<span style='color:green'>" + result[key] + "</span>." );
-    // lowercase ending with a ,
-    text = text.replace( key + ",", "<span style='color:green'>" + result[key] + "</span>," );
-    // lowercase ending with a !
-    text = text.replace( key + "!", "<span style='color:green'>" + result[key] + "</span>!" );
-    // lowercase ending with a ?
-    text = text.replace( key + "?", "<span style='color:green'>" + result[key] + "</span>?" );
-    // lowercase ending with a &
-    text = text.replace( key + "&", "<span style='color:green'>" + result[key] + "</span>&" );
-    // lowercase ending with a <
-    text = text.replace( key + "<", "<span style='color:green'>" + result[key] + "</span><" );
-    // uppercase ending with a space (in the beginning of a sentence)
-    text = text.replace( key.substr(0,1).toUpperCase() + key.substr(1) + " ", "<span style='color:green'>" + result[key].substr(0,1).toUpperCase() + result[key].substr(1) + "</span> " );
+    
+    var wordEndings = [",", "!", "?", "&", "<", " ", "."];
+
+
+    for (i = 0; i < wordEndings.length; i++) {
+
+      if (text.includes(key + wordEndings[i])) {
+          
+        text = text.replace( key + wordEndings[i], key + "<span style='color:green'> (" + result[key] + ")</span>" + wordEndings[i] );
+        
+      }
+      continue;
+
+      // uppercase ending with a space (in the beginning of a sentence)
+      text = text.replace( key.substr(0,1).toUpperCase() + key.substr(1) + " ", "<span style='color:green'>" + result[key].substr(0,1).toUpperCase() + result[key].substr(1) + "</span> " );
+    }
   }
   return text;
 }
 
-
 // main function
 $( document ).ready(function() {
-
+  
   needSync();
   
   var tags = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a'];
 
   chrome.storage.sync.get(null, function(result){
+
+  console.log(result);
+
     if (result.switcher) {
       for ( var i = 0; i < tags.length; i++ ) {
         $( tags[i] ).each (function() {
